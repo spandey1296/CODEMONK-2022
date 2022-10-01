9866d9f5a3ccd038fc95976a17e2cb753204a8d9
Slavecode <43001614+Slavecode@users.noreply.github.com>
2022-10-01 18:10:14 +0800

    Create umma.js

---
+++ umma.js
@@ -0,0 +1,28 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+function umma(url) {
+    return new Promise((resolve, reject) => {
+        axios.get(url)
+        .then((res) => {
+            let $ = cheerio.load(res.data)
+            let image = []
+            $('#article-content > div').find('img').each(function (a, b) {
+                image.push($(b).attr('src')) 
+            })
+            let hasil = {
+                title: $('#wrap > div.content-container.font-6-16 > h1').text().trim(),
+                author: {
+                    name: $('#wrap > div.content-container.font-6-16 > div.content-top > div > div.user-ame.font-6-16.fw').text().trim(),
+                    profilePic: $('#wrap > div.content-container.font-6-16 > div.content-top > div > div.profile-photo > img.photo').attr('src')
+
+                },
+                caption: $('#article-content > div > p').text().trim(),
+                media: $('#article-content > div > iframe').attr('src') ? [$('#article-content > div > iframe').attr('src')] : image,
+                type: $('#article-content > div > iframe').attr('src') ? 'video' : 'image',
+                like: $('#wrap > div.bottom-btns > div > button:nth-child(1) > div.text.font-6-12').text(),
+            }
+            resolve(hasil)
+        })
+    })
+}
