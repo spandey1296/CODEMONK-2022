97a4974db0eb9a4b71155d20293d5d362e24bda5
Alby Shinoj <91456674+albyshinoj@users.noreply.github.com>
2022-10-04 09:15:42 -0800

    prime number checker using python

---
+++ prime number checker/prime number checker.py
@@ -0,0 +1,28 @@
+print("************************Enjoy the Hacktoberfest******************************************************")
+# Program to check if a number is prime or not
+
+num =int(input("enter the number to be searched:"))
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# define a flag variable
+flag = False
+
+# prime numbers are greater than 1
+if num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+# check if flag is True
+if flag:
+    print(num, "is not a prime number")
+else:
+    print(num, "is a prime number")
+
+print("************************Enjoy the Hacktoberfest******************************************************")
